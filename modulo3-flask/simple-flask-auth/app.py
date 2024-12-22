e90066fa4e99c57db1909010f496ee918fd6655f
Igor Sousa <igor2001silva3@gmail.com>
2024-12-22 12:48:48 -0300

    Adding routes logout and CRUD user

--- modulo3-flask/simple-flask-auth/app.py
+++ modulo3-flask/simple-flask-auth/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -14,14 +14,14 @@ os.makedirs(app.instance_path, exist_ok=True)
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
-#view login
 login_manager.login_view = 'login'
-#Session <- conexão ativa
 
+# Rota de carregamento do usuário
 @login_manager.user_loader
 def load_user(user_id):
   return User.query.get(user_id)
 
+# Rota de login e autenticação
 @app.route('/login', methods=["POST"])
 def login():
   data = request.json
@@ -29,18 +29,84 @@ def login():
   password = data.get("password")
 
   if username and password:
-   # Login
     user = User.query.filter_by(username=username).first()
 
     if user and user.password == password:
       login_user(user)
       return jsonify({"message": "Autenticação realizada com sucesso!"})
 
-  return jsonify({"message": "Credenciais inválidas"}), 400
+  return jsonify({"message": "Credenciais inválidas!"}), 400
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-  return "Hello world"
+# Rota de logout e desautenticação
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logout realizado com sucesso!"})
+
+# Rota de criação de usuário
+@app.route('/user', methods=['POST'])
+@login_required
+def create_user():
+  data = request.get_json()
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password:
+    user_exist = User.query.filter_by(username=username).first()
+    if user_exist:
+      return jsonify({"message": "Nome de usuário já cadastrado no sistema. Por favor, informe os dados novamente!"})
+    
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({"message": "Usuário cadastrado com sucesso!"})
+
+  return jsonify({"message": "Dados inválidos!"}), 400
+
+# Rota de retorno de usuário
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+
+  if user:
+    return jsonify({"id": user.id, "username": user.username})
+  
+  return jsonify({"message": "Usuário não encontrado!"}), 404
+
+# Rota de atualização de senha usuário
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_password(id_user):
+  user = User.query.get(id_user)
+  data = request.get_json()
+  password = data.get('password')
+
+  if user and password:
+    user.password = password
+    db.session.commit()
+    
+    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso!"})
+      
+  return jsonify({"message": "Usuário não encontrado!"}), 404
+
+# Rota de exclusão de usuário
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+  user = User.query.get(id_user)
+
+  if id_user == current_user.id:
+    return jsonify({"message": "Não é permitido excluir o próprio usuário!"}), 403
+
+  if user:
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"Usuário {id_user} deletado com sucesso!"})
+  
+  return jsonify({"message": "Usuário não encontrado!"}), 404
 
 if __name__ == '__main__':
   app.run(debug=True)
