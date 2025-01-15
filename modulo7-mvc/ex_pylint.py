bdad262701cc9749b07140b99aa5a1c262783a9b
Igor Sousa <igor2001silva3@gmail.com>
2025-01-15 15:29:51 -0300

    Creating pylint

---
+++ modulo7-mvc/ex_pylint.py
@@ -0,0 +1,7 @@
+def minha_funcao() -> None:
+    ''' função para exibir um print '''
+    print("Olá mundo")
+
+# snake_case -> variáveis e funções em Python: my_variablem
+# cammelCase -> Não usa em python: minhaFuncao
+# PascalCase -> Classe em Python
