18e8d0bc841af2187160432211e1078279b33cb6
Igor Sousa <igor2001silva3@gmail.com>
2025-01-22 21:47:02 -0300

    Test: finishing tests in pets in pets_repository

--- modulo7-mvc/aula/src/models/sqlite/repositories/pets_repository_test.py
+++ modulo7-mvc/aula/src/models/sqlite/repositories/pets_repository_test.py
@@ -1,8 +1,12 @@
 # pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0321
 from unittest import mock
+import pytest
 from alchemy_mock.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pets import PetsTable
+
 from .pets_repository import PetsRepository
+
 
 class MockConnection:
     def __init__(self) -> None:
@@ -21,6 +25,17 @@ class MockConnection:
     def __enter__(self): return self
     def __exit__(self, exc_type, exc_val, exc_tb): pass
 
+class MockConnectionNoResult:
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
+
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_val, exc_tb): pass
+
 def test_list_pets():
     mock_connection = MockConnection()
     repo = PetsRepository(mock_connection)
@@ -33,3 +48,35 @@ def test_list_pets():
     mock_connection.session.filter.assert_not_called()
 
     assert response[0].name == "dog"
+
+def test_delete_pets():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+    repo.delete_pets("petName")
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
+    mock_connection.session.delete.assert_called_once()
+
+def test_list_pets_no_results():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    # Como é disparado um side_effect assim que acessa o método query, ele lança a exceção
+    # Sem executar o método all do list_pets
+    # Portanto, o session.all precisa ser assert not called, pois ele não será chamado
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
+
+def test_delete_pets_error():
+    mock_connection = MockConnectionNoResult()
+    repo = PetsRepository(mock_connection)
+
+    with pytest.raises(Exception):
+        repo.delete_pets("petName")
+
+    mock_connection.session.rollback.assert_called_once()
