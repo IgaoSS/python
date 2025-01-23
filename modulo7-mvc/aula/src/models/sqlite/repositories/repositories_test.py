59a3503f2d7fe3eb5086fc802f2db2d54b8de10a
Igor Sousa <igor2001silva3@gmail.com>
2025-01-22 22:08:24 -0300

    feat: Implementing people_repository

--- modulo7-mvc/aula/src/models/sqlite/repositories/repositories_test.py
+++ modulo7-mvc/aula/src/models/sqlite/repositories/repositories_test.py
@@ -2,8 +2,9 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
-#db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 # Esse é um teste de integração do sistema, apenas para verificar o retorno dos dados vindo do banco
 # Não é um teste unitário
@@ -18,3 +19,23 @@ def test_delete_pet():
     name = 'belinha'
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="Interação com o banco de dados")
+def test_insert_person():
+    first_name = "test name"
+    last_name = "test last"
+    age = 50
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+@pytest.mark.skip(reason="Interação com o banco de dados")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
+    print(response.pet_name)
