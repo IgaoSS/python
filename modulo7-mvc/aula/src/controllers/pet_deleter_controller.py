c3cdacd0dc1c846a57434ad5fb9ab4869c79d424
Igor Sousa <igor2001silva3@gmail.com>
2025-01-25 21:24:12 -0300

    feat: implementing pet_deleter_controller with tests

---
+++ modulo7-mvc/aula/src/controllers/pet_deleter_controller.py
@@ -0,0 +1,9 @@
+# pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0321
+from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
+
+class PetDeleterController:
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
+        self.__pet_repository = pet_repository
+
+    def delete(self, name: str) -> None:
+        self.__pet_repository.delete_pets(name)
--- modulo7-mvc/aula/src/controllers/pet_lister_controller.py
+++ modulo7-mvc/aula/src/controllers/pet_lister_controller.py
@@ -4,8 +4,8 @@ from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
 from src.models.sqlite.entities.pets import PetsTable
 
 class PetListerController:
-    def __init__(self, pets_repository: PetsRepositoryInterface) -> None:
-        self.__pets_repository = pets_repository
+    def __init__(self, pet_repository: PetsRepositoryInterface) -> None:
+        self.__pet_repository = pet_repository
 
     def list(self) -> Dict:
         pets = self.__get_pets_in_db()
@@ -13,7 +13,7 @@ class PetListerController:
         return response
 
     def __get_pets_in_db(self) -> List[PetsTable]:
-        pets = self.__pets_repository.list_pets()
+        pets = self.__pet_repository.list_pets()
         return pets
 
     def __format_response(self, pets: List[PetsTable]) -> Dict:
