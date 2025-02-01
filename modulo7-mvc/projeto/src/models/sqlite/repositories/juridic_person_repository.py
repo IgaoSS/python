d52899b540dee22ff9f553f7eef4d8625aa97cc5
Igor Sousa <igor2001silva3@gmail.com>
2025-02-01 14:32:01 -0300

    Implementing interface

--- modulo7-mvc/projeto/src/models/sqlite/repositories/juridic_person_repository.py
+++ modulo7-mvc/projeto/src/models/sqlite/repositories/juridic_person_repository.py
@@ -1,9 +1,10 @@
-# pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0301,E0401,E0611,R0913,R0917
+# pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0301,E0401,E0611,R0913,R0917,R0801
 from typing import List
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.juridic_person import JuridicPersonTable
+from src.models.sqlite.interfaces.client_repository import ClientRepositoryInterface
 
-class JuridicPersonRepository:
+class JuridicPersonRepository(ClientRepositoryInterface):
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
@@ -33,10 +34,10 @@ class JuridicPersonRepository:
             except NoResultFound:
                 return []
 
-    def withdraw_money(self, juridic_person_id: int, quantify: float):
+    def withdraw_money(self, person_id: int, quantify: float):
         try:
             daily_limit = 5000.00
-            balance = self.check_balance(juridic_person_id)
+            balance = self.check_balance(person_id)
 
             if quantify > daily_limit:
                 return "ERRO: Saque ultrapassa o limite permitido!"
@@ -45,31 +46,31 @@ class JuridicPersonRepository:
                 return "ERRO: Saldo não disponível para saque!"
 
             new_balance = balance - quantify
-            self.update_balance(juridic_person_id, new_balance)
+            self.update_balance(person_id, new_balance)
             return f"Saque de R$ {quantify} realizado com sucesso. Saldo atualizado: R$ {new_balance}"
         except Exception as exception:
             raise exception
 
-    def check_balance(self, juridic_person_id: int):
+    def check_balance(self, person_id: int):
         with self.__db_connection as database:
             try:
                 result = (
                     database.session
                     .query(JuridicPersonTable)
-                    .filter(JuridicPersonTable.id == juridic_person_id)
+                    .filter(JuridicPersonTable.id == person_id)
                     .first()
                 )
                 return result.saldo
             except NoResultFound:
                 return None
 
-    def update_balance(self, juridic_person_id: int, new_balance: float) -> None:
+    def update_balance(self, person_id: int, new_balance: float) -> None:
         with self.__db_connection as database:
             try:
                 result = (
                     database.session
                     .query(JuridicPersonTable)
-                    .filter(JuridicPersonTable.id == juridic_person_id)
+                    .filter(JuridicPersonTable.id == person_id)
                     .first()
                 )
                 result.saldo = new_balance
@@ -78,13 +79,13 @@ class JuridicPersonRepository:
                 database.session.rollback()
                 raise exception
 
-    def view_statement(self, juridic_person_id):
+    def view_statement(self, person_id):
         with self.__db_connection as database:
             try:
                 result = (
                     database.session
                     .query(JuridicPersonTable)
-                    .filter(JuridicPersonTable.id == juridic_person_id)
+                    .filter(JuridicPersonTable.id == person_id)
                     .first()
                 )
 
--- modulo7-mvc/projeto/src/models/sqlite/repositories/natural_person_repository.py
+++ modulo7-mvc/projeto/src/models/sqlite/repositories/natural_person_repository.py
@@ -1,10 +1,10 @@
-# pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0301,E0401,E0611,R0913,R0917
+# pylint: disable=C0114,C0115,C0116,W0703,C0209,E0015,C0304,C0301,E0401,E0611,R0913,R0917,R0801
 from typing import List
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.natural_person import NaturalPersonTable
-# Implementar a interface
+from src.models.sqlite.interfaces.client_repository import ClientRepositoryInterface
 
-class NaturalPersonRepository:
+class NaturalPersonRepository(ClientRepositoryInterface):
     def __init__(self, db_connection) -> None:
         self.__db_connection = db_connection
 
@@ -34,11 +34,11 @@ class NaturalPersonRepository:
             except NoResultFound:
                 return []
 
-    def withdraw_money(self, natural_person_id, quantify):
+    def withdraw_money(self, person_id, quantify):
         try:
             daily_limit = 2000.00
 
-            balance = self.check_balance(natural_person_id)
+            balance = self.check_balance(person_id)
 
             if quantify > daily_limit:
                 return "ERRO: Saque ultrapassa o limite permitido!"
@@ -47,32 +47,32 @@ class NaturalPersonRepository:
                 return "ERRO: Saldo não disponível para saque!"
 
             new_balance = balance - quantify
-            self.update_balance(natural_person_id, new_balance)
+            self.update_balance(person_id, new_balance)
             return f"Saque de R$ {quantify} realizado com sucesso. Saldo atualizado: R$ {new_balance}"
 
         except Exception as exception:
             raise exception
 
-    def check_balance(self, natural_person_id) -> NaturalPersonTable:
+    def check_balance(self, person_id) -> NaturalPersonTable:
         with self.__db_connection as database:
             try:
                 result = (
                     database.session
                         .query(NaturalPersonTable)
-                        .filter(NaturalPersonTable.id == natural_person_id)
+                        .filter(NaturalPersonTable.id == person_id)
                         .one()
                 )
                 return result.saldo
             except NoResultFound:
                 return None
 
-    def update_balance(self, natural_person_id, new_balance):
+    def update_balance(self, person_id, new_balance):
         with self.__db_connection as database:
             try:
                 result = (
                     database.session
                     .query(NaturalPersonTable)
-                    .filter(NaturalPersonTable.id == natural_person_id)
+                    .filter(NaturalPersonTable.id == person_id)
                     .first()
                 )
                 result.saldo = new_balance
@@ -81,13 +81,13 @@ class NaturalPersonRepository:
                 database.session.rollback()
                 raise exception
 
-    def view_statement(self, natural_person_id):
+    def view_statement(self, person_id):
         with self.__db_connection as database:
             try:
                 person = (
                     database.session
                     .query(NaturalPersonTable)
-                    .filter(NaturalPersonTable.id == natural_person_id)
+                    .filter(NaturalPersonTable.id == person_id)
                     .first()
                 )
 
