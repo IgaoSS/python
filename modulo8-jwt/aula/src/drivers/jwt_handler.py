7ce56f38db88a9b08a4431fedb49089d5712b272
Igor Sousa <igor2001silva3@gmail.com>
2025-02-11 22:31:16 -0300

    configs: adding .env to project

--- modulo8-jwt/aula/src/drivers/jwt_handler.py
+++ modulo8-jwt/aula/src/drivers/jwt_handler.py
@@ -1,20 +1,21 @@
-# pylint: disable=E0401,W0102
+# pylint: disable=E0401,W0102,C0301
 from typing import Dict
 from datetime import datetime, timedelta, timezone
 import jwt # type: ignore
+from src.configs.jwt_configs import jwt_infos
 
 class JwtHandler:
     def create_jwt_token(self, body: Dict = {}):
         token = jwt.encode(
             payload = {
-                'exp': datetime.now(timezone.utc) + timedelta(minutes=1),
+                'exp': datetime.now(timezone.utc) + timedelta(hours=int(jwt_infos["JWT_HOURS"])),
                 **body
             },
-            key="minhaChave",
-            algorithm="HS256"
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"]
         )
         return token
 
     def decode_jwt_token(self, token: str) -> Dict:
-        token_information = jwt.decode(token, key="minhaChave", algorithms="HS256")
+        token_information = jwt.decode(token, key=jwt_infos["KEY"], algorithms=jwt_infos["ALGORITHM"])
         return token_information
